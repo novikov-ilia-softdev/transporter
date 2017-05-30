514a010550907d479240211642200cf040978dc2
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-05-30 13:06:53 +0300

    2_async_tcp_client_server

--- test/2_async_tcp_client_server/client/client.cpp
+++ test/2_async_tcp_client_server/client/client.cpp
@@ -1,50 +1,53 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
-// g++ -o client.out client.cpp -lboost_system
+// g++ -o client.out client.cpp -lboost_system -lboost_thread
 
-void connect_handler( const boost::system::error_code& err);
+typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
+void connect_handler( socket_ptr socket, const boost::system::error_code& err);
+void write_handler( const boost::system::error_code& error, std::size_t bytes_transferred, socket_ptr socket);
 
 int main()
 {
 	boost::asio::io_service service;
-	boost::asio::ip::tcp::socket socket( service);
+	socket_ptr socket( new boost::asio::ip::tcp::socket( service));
 
 	const std::string remoteAddress = "127.0.0.1";
 	const int remotePort = 2001;
 	boost::asio::ip::tcp::endpoint endpoint( boost::asio::ip::address::from_string( remoteAddress.c_str()), remotePort);
 
-	socket.async_connect( endpoint, connect_handler);
+	socket->async_connect( endpoint, boost::bind( connect_handler, socket, boost::asio::placeholders::error));
 	service.run();
 }
 
-void connect_handler( const boost::system::error_code& err)
+void connect_handler( socket_ptr socket, const boost::system::error_code& err)
 {
-	if( !err)
-	{
-		std::cout << "connected!" << std::endl;
-		//const std::string str = "Hello, world!";
-		//boost::asio::write( socket, boost::asio::buffer( str.c_str(), str.length()));
-		//socket.close();
-	}
-	else
+	if( err)
 	{
 		std::cout << "error connection!" << std::endl;
+		return;
 	}
+
+	std::cout << "connected!" << std::endl;
+	const std::string str = "Hello, world!";
+
+	//boost::this_thread::sleep( boost::posix_time::milliseconds(5000));
+
+	boost::asio::async_write( *socket,
+							  boost::asio::buffer( str.c_str(), str.length()),
+							  boost::bind( write_handler, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred, socket));
 }
 
-/*
-
-try
+void write_handler( const boost::system::error_code& error, std::size_t bytes_transferred, socket_ptr socket)
+{
+	if( error)
 	{
-
-
-	}
-	catch( boost::system::system_error& err)
-	{
-		std::cout << "exception:" << std::endl;
-		std::cout << err.what() << std::endl;
+		std::cout << "error write_handler!" << std::endl;
 	}
 
-
- */
+	std::cout << "write_handler" << std::endl;
+	std::cout << "bytes_transferred: " << bytes_transferred << std::endl;
+	socket->close();
+}
--- test/2_async_tcp_client_server/server/server.cpp
+++ test/2_async_tcp_client_server/server/server.cpp
@@ -5,33 +5,59 @@
 // g++ -o server.out server.cpp -lboost_system -lboost_thread
 
 typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
-void start_accept( socket_ptr socket, boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_service& service)
-void handle_accept( socket_ptr socket, boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_service& service, const boost::system::error_code& err)
+typedef boost::shared_ptr<boost::asio::io_service> service_ptr;
+typedef boost::shared_ptr<boost::asio::ip::tcp::acceptor> acceptor_ptr;
+
+void start_accept( socket_ptr socket, acceptor_ptr acceptor, service_ptr service);
+void handle_accept( socket_ptr socket, acceptor_ptr acceptor, service_ptr service, const boost::system::error_code& err);
+void handle_read( const boost::system::error_code& error, std::size_t bytes_transferred, socket_ptr socket);
+
+char data[ 512];
 
 int main()
 {
 	const int localPort = 2001;
 
-	boost::asio::io_service service;
+	service_ptr service( new boost::asio::io_service());
 	boost::asio::ip::tcp::endpoint endpoint( boost::asio::ip::tcp::v4(), localPort);
-	boost::asio::ip::tcp::acceptor acceptor( service, endpoint);
+	acceptor_ptr acceptor( new boost::asio::ip::tcp::acceptor( *service, endpoint));
+	socket_ptr socket( new boost::asio::ip::tcp::socket( *service));
 
-	socket_ptr socket( new boost::asio::ip::tcp::socket( service));
 	start_accept( socket, acceptor, service);
-	service.run();
+	service->run();
 }
 
-void start_accept( socket_ptr socket, boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_service& service)
+void start_accept( socket_ptr socket, acceptor_ptr acceptor, service_ptr service)
 {
-	acceptor.async_accept( boost::bind( handle_accept, socket, _1, acceptor, _2, service, _3));
+	acceptor->async_accept( *socket, boost::bind( handle_accept, socket, acceptor, service, boost::asio::placeholders::error));
 }
 
-void handle_accept( socket_ptr socket, boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_service& service, const boost::system::error_code& err)
+void handle_accept( socket_ptr socket, acceptor_ptr acceptor, service_ptr service, const boost::system::error_code& err)
 {
-	if( !err)
+	if( err)
 	{
-		std::cout << "accepted!" << std::endl;
-		socket_ptr socket( new boost::asio::ip::tcp::socket( service));
-		start_accept( socket, acceptor);
+		std::cout << "error acception!" << std::endl;
+		return;
 	}
+
+	std::cout << "accepted!" << std::endl;
+	socket_ptr newSocket( new boost::asio::ip::tcp::socket( *service));
+	start_accept( newSocket, acceptor, service);
+
+	socket->async_read_some( boost::asio::buffer( data),
+							 boost::bind( handle_read, boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred, socket));
+}
+
+void handle_read( const boost::system::error_code& error, std::size_t bytes_transferred, socket_ptr socket)
+{
+	if( error)
+	{
+		std::cout << "error handle_read!" << std::endl;
+	}
+
+	std::cout << "handle_read" << std::endl;
+	std::cout << "bytes_transferred: " << bytes_transferred << std::endl;
+	std::cout << "data: " << data << std::endl;
+
+	socket->close();
 }
