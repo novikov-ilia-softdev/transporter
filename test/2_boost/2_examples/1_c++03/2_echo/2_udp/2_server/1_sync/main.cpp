e930374e4d52e69d3cbe3bf22933b04675c0ece2
smartnlgilianovikov <smartnlg.ilia.novikov@gmail.com>
2017-06-12 13:09:33 +0300

    echo udp server sync formatted

--- test/2_boost/2_examples/1_c++03/2_echo/2_udp/2_server/1_sync/main.cpp
+++ test/2_boost/2_examples/1_c++03/2_echo/2_udp/2_server/1_sync/main.cpp
@@ -1,45 +1,37 @@
 // g++ -o server.out main.cpp -lboost_system
 
-#include <cstdlib>
 #include <iostream>
 #include <boost/asio.hpp>
 
-using boost::asio::ip::udp;
+enum { maxLength = 1024 };
 
-enum { max_length = 1024 };
-
-void server(boost::asio::io_service& io_service, unsigned short port)
+void server( boost::asio::io_service& io_service, unsigned short port)
 {
-  udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
-  for (;;)
-  {
-    char data[max_length];
-    udp::endpoint sender_endpoint;
-    size_t length = sock.receive_from(
-        boost::asio::buffer(data, max_length), sender_endpoint);
-    sock.send_to(boost::asio::buffer(data, length), sender_endpoint);
-  }
+	boost::asio::ip::udp::socket sock( io_service, boost::asio::ip::udp::endpoint( boost::asio::ip::udp::v4(), port));
+	for (;;)
+	{
+		char data[ maxLength];
+		boost::asio::ip::udp::endpoint senderEndpoint;
+		size_t length = sock.receive_from( boost::asio::buffer( data, maxLength), senderEndpoint);
+		sock.send_to( boost::asio::buffer( data, length), senderEndpoint);
+	}
 }
 
-int main(int argc, char* argv[])
+int main( int argc, char* argv[])
 {
-  try
-  {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: blocking_udp_echo_server <port>\n";
-      return 1;
-    }
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: blocking_boost::asio::ip::udp_echo_server <port>\n";
+			return 1;
+		}
 
-    boost::asio::io_service io_service;
-
-    using namespace std; // For atoi.
-    server(io_service, atoi(argv[1]));
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+		boost::asio::io_service ioService;
+		server( ioService, atoi(argv[1]));
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
 }
