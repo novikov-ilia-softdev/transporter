ecf0aab39e3642aea33f5e32d4838d0ce2ef572a
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-06-26 11:48:08 +0300

    serverargs refactor

--- src/args/argsparser.cpp
+++ src/args/argsparser.cpp
@@ -3,6 +3,8 @@
 
 const int ArgsParser::MODE_INDEX = 1;
 const int ArgsParser::SERVER_ARGS_COUNT = 4;
+const int ArgsParser::SERVER_PORT_INDEX = 2;
+const int ArgsParser::SERVER_TRANSPORT_INDEX = 3;
 const int ArgsParser::CLIENT_ARGS_COUNT = 5;
 
 ArgsParser::ArgsParser( int argc, char* argv[]):
@@ -12,18 +14,18 @@ ArgsParser::ArgsParser( int argc, char* argv[]):
 
 bool ArgsParser::isClient() const
 {
-	// -c ip:port tcp/udp file
+	// transporter -c ip:port tcp/udp file
 	return ( argc_ == CLIENT_ARGS_COUNT &&
 			 std::string( argv_[ MODE_INDEX]) == "-c");
 }
 
 ServerArgs* ArgsParser::getServerArgs() const
 {
-	// -s port tcp/udp
+	// transporter -s port tcp/udp
 	if( argc_ == SERVER_ARGS_COUNT &&
 		std::string( argv_[ MODE_INDEX]) == "-s")
 	{
-		return new ServerArgs( argc_, argv_);
+		return new ServerArgs( argv_[ SERVER_PORT_INDEX], argv_[ SERVER_TRANSPORT_INDEX]);
 	}
 
 	return 0;
