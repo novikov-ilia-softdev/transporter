06b655aee53cf166e3875e493dc3b3015d76328e
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-06-28 11:32:57 +0300

    added accept in TCPServer

--- src/net/tcpserver.cpp
+++ src/net/tcpserver.cpp
@@ -1,12 +1,33 @@
 #include "tcpserver.h"
 
 TCPServer::TCPServer( ServerArgsPtr serverArgsPtr):
-	IServer( serverArgsPtr)
+	IServer( serverArgsPtr),
+	serverArgsPtr_( serverArgsPtr),
+	acceptor_( ioService_, boost::asio::ip::tcp::endpoint( boost::asio::ip::tcp::v4(), 12345)),
+	socket_( ioService_)
 {
 	std::cout << "TCPServer" << std::endl;
+	accept_();
 }
 
 void TCPServer::run()
 {
-	std::cout << "TCPServer::run" << std::endl;
+	std::cout << "TCPServer::run before" << std::endl;
+	ioService_.run();
+	std::cout << "TCPServer::run after" << std::endl;
+}
+
+void TCPServer::accept_()
+{
+	acceptor_.async_accept( socket_,
+							[ this]( boost::system::error_code ec)
+							{
+								if( !ec)
+								{
+									//std::make_shared<Session>( std::move( socket_))->start();
+									std::cout << "connected!" << std::endl;
+								}
+
+								accept_();
+							});
 }
--- src/net/tcpserver.h
+++ src/net/tcpserver.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <iostream>
+#include <boost/asio.hpp>
 #include "iserver.h"
 
 class TCPServer: public IServer{
@@ -11,7 +12,13 @@ public:
 	void run();
 
 private:
+	void accept_();
+
+private:
 	ServerArgsPtr serverArgsPtr_;
+	boost::asio::io_service ioService_;
+	boost::asio::ip::tcp::acceptor acceptor_;
+	boost::asio::ip::tcp::socket socket_;
 };
 
 typedef std::shared_ptr<TCPServer> TCPServerPtr;
