495866d4c8e66300ac30ab3316258e0dfece2322
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-08-07 16:12:24 +0300

    added tcp test

--- test/transporter/test.h
+++ test/transporter/test.h
@@ -1,13 +1,37 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <fstream>
+
 class Test : public ::testing::Test
 {
 };
 
-TEST_F( Test, 1)
+/*
+TODO:
+1) short path
+2) system -> boost_process
+3) create file
+4) compare file
+5) delete file
+6) udp
+*/
+
+TEST_F( Test, tcp)
 {
-	EXPECT_TRUE( 1 == 1);
+	system( "~/workspace/transporter/build/src/transporter -s 12345 tcp &");
+	sleep( 1);
+	system( "~/workspace/transporter/build/src/transporter -c 127.0.0.1 12345 tcp ~/workspace/transporter/src/main.cpp");
+	system( "killall -15 transporter");
+
+	std::ifstream srcFile( "/home/ilia-novikov/workspace/transporter/src/main.cpp");
+	std::string srcFileContent( (std::istreambuf_iterator<char>(srcFile)), (std::istreambuf_iterator<char>()));
+
+	std::ifstream dstFile( "main.cpp");
+	std::string dstFileContent( (std::istreambuf_iterator<char>(dstFile) ),(std::istreambuf_iterator<char>()    ) );
+
+	EXPECT_TRUE( srcFileContent == dstFileContent);
+	system( "rm main.cpp");
 }
 
 #endif // TEST_H
