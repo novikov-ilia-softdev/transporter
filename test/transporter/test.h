9bbf645980ed65468ea526e7eb8180a7e5f32b28
ilia.novikov <smartnlg.ilia.novikov@gmail.com>
2017-08-08 16:22:57 +0300

    test refactoring

--- test/transporter/test.h
+++ test/transporter/test.h
@@ -5,6 +5,50 @@
 
 class Test : public ::testing::Test
 {
+public:
+	static void runTCPServer()
+	{
+		system( "../../build/src/transporter -s 12345 tcp &");
+		sleep( 1);
+	}
+
+	static void runTCPClient()
+	{
+		system( "../../build/src/transporter -c 127.0.0.1 12345 tcp ../../src/main.cpp");
+	}
+
+	static void runUDPServer()
+	{
+		system( "../../build/src/transporter -s 12345 udp &");
+		sleep( 1);
+	}
+
+	static void runUDPClient()
+	{
+		system( "../../build/src/transporter -c 127.0.0.1 12345 udp ../../src/main.cpp");
+		sleep( 1);
+	}
+
+	static void stopAll()
+	{
+		system( "killall -15 transporter");
+	}
+
+	static void compareFiles()
+	{
+		std::ifstream srcFile( "../../src/main.cpp");
+		std::string srcFileContent( (std::istreambuf_iterator<char>(srcFile)), (std::istreambuf_iterator<char>()));
+
+		std::ifstream dstFile( "main.cpp");
+		std::string dstFileContent( (std::istreambuf_iterator<char>(dstFile) ),(std::istreambuf_iterator<char>()    ) );
+
+		EXPECT_TRUE( srcFileContent == dstFileContent);
+	}
+
+	static void deleteDstFile()
+	{
+		system( "rm main.cpp");
+	}
 };
 
 /*
@@ -18,37 +62,21 @@ TODO:
 
 TEST_F( Test, tcp)
 {
-	system( "~/workspace/transporter/build/src/transporter -s 12345 tcp &");
-	sleep( 1);
-	system( "~/workspace/transporter/build/src/transporter -c 127.0.0.1 12345 tcp ~/workspace/transporter/src/main.cpp");
-	system( "killall -15 transporter");
+	Test::runTCPServer();
+	Test::runTCPClient();
+	Test::stopAll();
+	Test::compareFiles();
+	Test::deleteDstFile();
 
-	std::ifstream srcFile( "/home/ilia-novikov/workspace/transporter/src/main.cpp");
-	std::string srcFileContent( (std::istreambuf_iterator<char>(srcFile)), (std::istreambuf_iterator<char>()));
-
-	std::ifstream dstFile( "main.cpp");
-	std::string dstFileContent( (std::istreambuf_iterator<char>(dstFile) ),(std::istreambuf_iterator<char>()    ) );
-
-	EXPECT_TRUE( srcFileContent == dstFileContent);
-	system( "rm main.cpp");
 }
 
 TEST_F( Test, udp)
 {
-	system( "~/workspace/transporter/build/src/transporter -s 12345 udp &");
-	sleep( 1);
-	system( "~/workspace/transporter/build/src/transporter -c 127.0.0.1 12345 udp ~/workspace/transporter/src/main.cpp");
-	sleep( 1);
-	system( "killall -15 transporter");
-
-	std::ifstream srcFile( "/home/ilia-novikov/workspace/transporter/src/main.cpp");
-	std::string srcFileContent( (std::istreambuf_iterator<char>(srcFile)), (std::istreambuf_iterator<char>()));
-
-	std::ifstream dstFile( "main.cpp");
-	std::string dstFileContent( (std::istreambuf_iterator<char>(dstFile) ),(std::istreambuf_iterator<char>()    ) );
-
-	EXPECT_TRUE( srcFileContent == dstFileContent);
-	system( "rm main.cpp");
+	Test::runUDPServer();
+	Test::runUDPClient();
+	Test::stopAll();
+	Test::compareFiles();
+	Test::deleteDstFile();
 }
 
 #endif // TEST_H
